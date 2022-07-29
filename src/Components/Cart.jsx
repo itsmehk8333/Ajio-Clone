b65f982ca9dcccd32c6b0fc0fe617549cd1050dc
Harikrishna Gunnala <harikrishnagunnala833@gmail.com>
2022-07-29 20:16:59 +0530

    third commit

--- src/Components/Cart.jsx
+++ src/Components/Cart.jsx
@@ -15,59 +15,61 @@ var price = 0
 cart.map((e)=>{
 price += parseInt(e.price) 
 })
-console.log(price)
-    console.log(price)
+console.log(cart.length)
 
 
-    console.log(cart)
-    return (
-    <div  className='Cart-Container'>
-    <div>
-    {
-      cart.length > 0 ?
-      cart.map((e)=>(
-        <div className='cart-item'>
-        <img  src={e.img} className="cart-img" />
-         <div className='cart-details'>
-         <h4> Price : {e.price} <span> &#8377;</span></h4>
-     <div className='del-div'>
-     <h4 className='item-size'>Size :  {e.size}</h4>
-        
-         
-     <button  className='del-btn' onClick={()=>{
-       console.log(cart.findIndex( i => i.id  == e.id));
-       Dispatch(DeleteItem(cart.findIndex( i => i.id  == e.id)))
-     }}> <img src={delimg}  className="delimg" /></button>
-     </div>
-         </div>
-        </div>
   
-        )) : <h1>Cart is empty</h1>
-    } 
+    return (
+    <div className='Main-Container'>
+    {
+      cart.length > 0 ? <div  className='main-container'>
+      
+      <div className='cart-items'>
+
+    {
+
+      cart.map((e)=>(
+      <div  className='cart-item'>
+<img src={e.img}  className="cart-img"/>
+<div  className='details-div'>
+<p className='item-price'>  Price : {e.price} <span> &#8377;</span></p>
+<p className='Size'>Size  :{e.size}   <button
+onClick={()=>{
+  Dispatch(DeleteItem(cart.findIndex( i => i.id  == e.id)))
+}}
+className="delete-item"
+><img src={delimg} /></button></p>
+
+</div>
+
+      </div>
+
+      ))
+    }
 
 
-    
-
-    </div>
-
-    <div  className='Payment'>
-
-    <div className='payment'>
-    <h2>Order Details</h2>
+      </div>
+      <div className='payment-div'>
+      
+      <h2 style={{margin:"0 0 20px  50px"}}>Order Details</h2>
      <p className='bagTotal'>Bagtotal <span style={{marginLeft:'10rem'}}>{price} /Rs</span></p>
      <p className='bagTotal'>Bagdiscount <span style={{marginLeft:'8rem'}}>500 /Rs</span></p>
      <p className='bagTotal'>Delivery <span style={{marginLeft:'11rem',color:"green"}}>Free</span></p>
      <p className='bagTotal'>Total Amount <span style={{marginLeft:'8rem'}}>{price - 500} /RS</span></p>
+     <button className='btn bg-success' id='checkout-btn'>Check out</button>
 
 
-   
-     </div>
-  <button className='btn bg-success' id='checkout-btn'>Check out</button>
-    
-    
-    </div>
-    
-    
+      </div>
+      
+      
+      </div>
+      
+      : <div>
+      <h1>Cart is Empty</h1>
+      
+      </div>
+    }
+  
     </div>
   )
 }
--- src/Pages/Homepage.jsx
+++ src/Pages/Homepage.jsx
@@ -5,6 +5,10 @@ import scorll from '../Images/Scorll.png'
 import scroll2 from '../Images/Scroll2.png'
 import scroll3 from '../Images/Scroll3.png';
 import scroll4 from '../Images/Scroll4.png';
+import Sc7 from '../Images/Sc7.png'
+import Sc5 from '../Images/Sc5.png'
+import Sc6 from '../Images/Sc6.png'
+import Sc8 from '../Images/Sc8.png'
 import '../Pages/Homepage.css'
 
 const fadeImages = [
@@ -26,19 +30,64 @@ const fadeImages = [
 
 function Homepage() {
   return (
-    <div>
-    <div className="slide-container">
-    <Fade>
-      {fadeImages.map((fadeImage, index) => (
-        <div className="each-fade" key={index}>
-          <div className="image-container">
-            <img src={fadeImage.url} />
-          </div>
-          <h2>{fadeImage.caption}</h2>
-        </div>
-      ))}
-    </Fade>
+    <div className='Home-Container'>
+     
+<div id="demo" class="carousel slide" data-bs-ride="carousel">
+
+<div class="carousel-indicators">
+  <button type="button" data-bs-target="#demo" data-bs-slide-to="0" class="active"></button>
+  <button type="button" data-bs-target="#demo" data-bs-slide-to="1"></button>
+  <button type="button" data-bs-target="#demo" data-bs-slide-to="2"></button>
+</div>
+
+
+<div class="carousel-inner">
+  <div class="carousel-item active">
+    <img src={scorll} alt="Los Angeles" class="d-block w-100" />
   </div>
+  <div class="carousel-item">
+    <img src={scroll2} alt="Chicago" class="d-block w-100" />
+  </div>
+  <div class="carousel-item">
+    <img src={scroll3} alt="New York" class="d-block w-100" />
+  </div>
+</div>  
+<button class="carousel-control-prev" type="button" data-bs-target="#demo" data-bs-slide="prev">
+  <span class="carousel-control-prev-icon"></span>
+</button>
+<button class="carousel-control-next" type="button" data-bs-target="#demo" data-bs-slide="next">
+  <span class="carousel-control-next-icon"></span>
+</button>
+</div>
+    
+ 
+<div id="demo1" class="carousel slide" data-bs-ride="carousel">
+
+  <div class="carousel-indicators">
+    <button type="button" data-bs-target="#demo" data-bs-slide-to="0" class="active"></button>
+    <button type="button" data-bs-target="#demo" data-bs-slide-to="1"></button>
+    <button type="button" data-bs-target="#demo" data-bs-slide-to="2"></button>
+  </div>
+
+
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src={Sc8} alt="Los Angeles" class="d-block w-100" />
+    </div>
+    <div class="carousel-item">
+      <img src={Sc6} alt="Chicago" class="d-block w-100" />
+    </div>
+    <div class="carousel-item">
+      <img src={Sc7} alt="New York" class="d-block w-100" />
+    </div>
+  </div>  
+  <button class="carousel-control-prev" type="button" data-bs-target="#demo1" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon"></span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#demo1" data-bs-slide="next">
+    <span class="carousel-control-next-icon"></span>
+  </button>
+</div>
 
     </div>
   )
